c365498103fa29b12f99b217c4903fcdc5b362b2
Afghan H.R <136172306+afganrofiqi@users.noreply.github.com>
2023-06-21 13:55:09 +0700

    Update nota.php

--- parfum_shop/nota.php
+++ parfum_shop/nota.php
@@ -193,7 +193,7 @@ $detail = $ambl->fetch_assoc();
 
   </main><!-- End #main -->
 
-  <?php include 'footer.php'; ?>
+  <?php include 'footer.html'; ?>
   <!-- End Footer -->
 
   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
