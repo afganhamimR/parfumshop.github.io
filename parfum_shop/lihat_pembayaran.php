14aa246378e98a8d585cb07b27aad1c6e4343711
Afghan H.R <136172306+afganrofiqi@users.noreply.github.com>
2023-06-21 13:54:35 +0700

    Update lihat_pembayaran.php

--- parfum_shop/lihat_pembayaran.php
+++ parfum_shop/lihat_pembayaran.php
@@ -218,7 +218,7 @@ if (empty($detbay))
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <?php include 'footer.php'; ?>
+  <?php include 'footer.html'; ?>
   <!-- End Footer -->
 
   <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
